cce33e854e1117717881d2c92a975a48ea3206e5
1 <1@1->
2023-03-14 21:49:33 +0200

    Добавьте файлы проекта.

---
+++ Laba1_3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography;
+using Laba1_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Введення даних
+        string IntValue1;
+        Ryadky r1 = new Ryadky();
+        Ryadky r2 = new Ryadky();
+        while(r1.check())
+        {
+            Console.Write("Введiть перший рядок(0-9):");
+            r1.ryadok = Console.ReadLine();
+        }
+        while (r2.check())
+        {
+            Console.Write("Введiть другий рядок(0-9):");
+            r2.ryadok = Console.ReadLine();
+        }
+        Console.Write("Введiть значення типу int:");
+        IntValue1 = Console.ReadLine();
+        int IntValue2 = Convert.ToInt32(IntValue1);
+
+        //Перевантаження операторів
+
+        //String * int
+        Ryadky r6 = r1 * IntValue2;
+
+        //int * String
+        Ryadky r7 = IntValue2 * r1;
+
+        //String+String
+        Ryadky r3 = r1 + r2;
+
+        //String == String
+        bool r4 = r1 == r2;
+
+        //String--
+        Ryadky r5 = --r1;
+
+        //Виведення
+
+        //String * int
+        Console.WriteLine("_______________");
+        Console.WriteLine("String * int");
+        Console.WriteLine("Ryadky r6 = r1 * intValue");
+        Console.WriteLine("r6.ryadok = " + r6.ryadok);
+
+        //int * String
+        Console.WriteLine("_______________");
+        Console.WriteLine("int * String");
+        Console.WriteLine("Ryadky r7 = intValue * r1");
+        Console.WriteLine("r7.ryadok = " + r7.ryadok);
+
+        //String+String
+        Console.WriteLine("_______________");
+        Console.WriteLine("String + String"); 
+        Console.WriteLine("Ryadky r3 = r1+r2");
+        Console.WriteLine("r3.ryadok = "+r3.ryadok);
+
+        //String == String
+        Console.WriteLine("_______________");
+        Console.WriteLine("String == String");
+        Console.WriteLine("r4 = r1==r2");
+        Console.WriteLine("r4 = "+r4);
+
+        //String--
+        Console.WriteLine("_______________");
+        Console.WriteLine("String--");
+        Console.WriteLine("Ryadky r5 = --r1");
+        Console.WriteLine("r5.ryadok = " + r5.ryadok);
+
+        //Повернути якщо будь-який із закритих полів класу не дорівнює 0
+        Console.WriteLine("_______________");
+        Console.WriteLine("Повернути true якщо будь-який iз закритих полiв класу не дорiвнює 0");
+        if (r1) { Console.WriteLine(true);}
+        else Console.WriteLine(false);
+    }
+}
---
+++ Laba1_3/Ryadky.cs
@@ -0,0 +1,116 @@
+﻿namespace Laba1_3
+{
+    internal class Ryadky
+    {
+        public string ryadok = "";
+
+        private bool result = true;
+        //Конструктор
+        public Ryadky()
+        {
+
+        }
+        //Перевірка(Видає true якщо цифрові символи)
+        public bool check()
+        {
+
+            for (int i = 0; i < ryadok.Length; i++)
+            {
+                if ((ryadok[i] >= '0' && ryadok[i] <= '9'))
+                {
+                    result = false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            if (result == false)
+            {
+                return false;
+            }
+            else return true;
+
+        }
+        //Метод обчислення довжини
+        public int length()
+        {
+            return ryadok.Length;
+        }
+        //Метод обернення рядка
+        public void invert()
+        {
+            string ryadokTmp = "";
+            for (int i = ryadok.Length - 1; i >= 0; i--)
+            {
+                ryadokTmp += ryadok[i];
+            }
+            ryadok = ryadokTmp;
+        }
+        //Метод додавання символу в рядок
+        public void add(string c)
+        {
+            if (c[0] >= '0' && c[0] <= '9' && c.Length == 1)
+            {
+                ryadok += c;
+            }
+        }
+        //Метод зміни рядка
+        public void change(string ryadokTmp)
+        {
+            ryadok = ryadokTmp;
+        }
+
+
+
+        //String* int
+        public static Ryadky operator *(Ryadky r1, int i)
+        {
+            return new Ryadky { ryadok = r1.ryadok + i };
+        }
+        //int * String
+        public static Ryadky operator *(int i, Ryadky r1)
+        {
+            return new Ryadky { ryadok = i + r1.ryadok };
+        }
+        //String+String
+        public static Ryadky operator +(Ryadky r1, Ryadky r2)
+        {
+            return new Ryadky { ryadok = r1.ryadok + r2.ryadok };
+        }
+        //String==String
+        public static bool operator ==(Ryadky r1, Ryadky r2)
+        {
+            return r1.ryadok == r2.ryadok;
+        }
+        public static bool operator !=(Ryadky r1, Ryadky r2)
+        {
+            return r1.ryadok != r2.ryadok;
+        }
+        //String--
+        public static Ryadky operator --(Ryadky r1)
+        {
+            r1.ryadok = r1.ryadok.Substring(0, r1.ryadok.Length - 1);
+            return new Ryadky
+            {
+                ryadok = r1.ryadok
+            };
+        }
+        public static bool operator true(Ryadky r1)
+        {
+            return r1.ryadok != "";
+        }
+        public static bool operator false(Ryadky r1)
+        {
+            return r1.ryadok == "";
+        }
+
+
+
+        //деструктор
+        ~Ryadky()
+        {
+
+        }
+    }
+}
